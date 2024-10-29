9d055ba7bb868fa379f2834d074314206020c566
Aloterra <ripallol.541@yandex.ru>
2024-10-29 22:00:58 +0300

    Добавление странички пользователя, реализация отображения книг

--- Lab3/UserForm.Designer.cs
+++ Lab3/UserForm.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewBooks
+            // 
+            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBooks.Location = new System.Drawing.Point(139, 54);
+            this.dataGridViewBooks.Name = "dataGridViewBooks";
+            this.dataGridViewBooks.Size = new System.Drawing.Size(520, 320);
+            this.dataGridViewBooks.TabIndex = 0;
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewBooks);
+            this.Name = "UserForm";
             this.Text = "UserForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewBooks;
     }
 }
--- Lab3/UserForm.cs
+++ Lab3/UserForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,42 @@ namespace Lab3
 {
     public partial class UserForm : Form
     {
+        private MySqlConnection connection;
+
         public UserForm()
         {
             InitializeComponent();
+            // Инициализируем соединение с базой данных
+            DatabaseHelper dbHelper = new DatabaseHelper();
+            connection = new MySqlConnection(dbHelper.GetConnectionString());
+            LoadBooksData(); // Загружаем данные книг при инициализации формы
+        }
+
+        private void LoadBooksData()
+        {
+            try
+            {
+                // Открываем соединение
+                connection.Open();
+                // SQL-запрос для получения данных из таблицы books
+                string query = "SELECT * FROM books";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                DataTable booksTable = new DataTable();
+
+                // Заполняем DataTable данными из базы данных
+                adapter.Fill(booksTable);
+                // Устанавливаем источник данных для DataGridView
+                dataGridViewBooks.DataSource = booksTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке данных: {ex.Message}");
+            }
+            finally
+            {
+                // Закрываем соединение
+                connection.Close();
+            }
         }
     }
 }
